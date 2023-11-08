080aba4d4a138915d2c66bbe93453667f154ee25
leeseokku <127724468+leeseokku@users.noreply.github.com>
2023-11-08 13:50:34 +0900

    Update ex3-1.sh

--- src/ex3-1.sh
+++ src/ex3-1.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-# 사용자에게 숫자를 입력받습니다.
-echo "몇 번 출력하시겠습니까?"
-read count
+if [ -n "$1" ] 
+then
+    count=$1
+    while [ $count -ne 0 ]
+    do
+        echo "hello world"
+        count=$(($count - 1))
+    done
+else
+    while [ 1 ]
+    do
+        echo "hello world"
+    done
+fi
 
-# 입력받은 숫자만큼 반복하여 "Hello world"를 출력합니다.
-for ((i=0; i<$count; i++))
-do
-    echo "Hello world"
-done
-
+exit 0
